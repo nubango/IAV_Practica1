c98f75f84ec90db009b67e24166aa10316150603
Unknown <gonalba@ucm.es>
2020-02-26 12:16:24 +0100

    Following camera (by Marcos)

---
+++ Assets/Scripts/camFollow.cs
@@ -0,0 +1,30 @@
+﻿namespace UCM.IAV.Movimiento
+{
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class camFollow : MonoBehaviour
+    {
+        public GameObject personaje;
+        private Vector3 posicionRelativa;
+        // Start is called before the first frame update
+        void Start()
+        {
+            posicionRelativa = transform.position - personaje.transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void LateUpdate()
+        {
+            transform.position = personaje.transform.position + posicionRelativa;
+        }
+    }
+}
