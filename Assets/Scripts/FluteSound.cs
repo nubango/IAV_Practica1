e8e68791ca8f33759b573cb068e4bfb2c10966e9
Nuria <nubango@ucm.es>
2020-02-27 00:18:53 +0100

    Flute Sound

---
+++ Assets/Scripts/FluteSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class FluteSound : MonoBehaviour
+{
+    AudioSource audioData;
+
+    public void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            audioData.Play(0);
+            Debug.Log("started");
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            audioData.Pause();
+        }
+    }
+}
